70be6ef29f5c6498286efc205483435ac7e1e310
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-08 17:12:38 +0600

    Executing Pool and creating tables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { type Application, type Request, type Response } from "express"
-import {Pool} from "pg"
+import { Pool } from "pg"
 
 const app: Application = express()
 const port = 5000
@@ -9,9 +9,32 @@ app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
 
+// connection between project and neon database
 const pool = new Pool({
-  connectionString:'postgresql://neondb_owner:npg_C1oh6gTAGzRn@ep-twilight-dew-aog6yrcd-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+  connectionString: 'postgresql://neondb_owner:npg_C1oh6gTAGzRn@ep-twilight-dew-aog6yrcd-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 })
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `)
+    console.log("Database Connected Successfully")
+  } catch (error) {
+
+  }
+}
+initDB();
 
 // get method
 app.get('/', (req: Request, res: Response) => {
