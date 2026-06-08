8afc86120332c791aadd402da7f9ee1ca2afcf6b
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-08 12:25:31 +0600

    Understanding the express Request and Response

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app: Application = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Express Server')
+// use middler for post method.
+app.use(express.json())
+
+// get method
+app.get('/', (req: Request, res: Response) => {
+  // res.send('Express Server')
+  res.status(200).json({ 'message': 'Express Server', 'author': 'Next Level' })
+});
+
+// post method
+app.post('/', async (req: Request, res: Response) => {
+  console.log(req.body)
 })
 
 app.listen(port, () => {
