07e53d364b0309c8af496c9d2afed7546a74b08e
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-08 18:09:31 +0600

    Creating our first User with POST method

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const initDB = async () => {
       CREATE TABLE IF NOT EXISTS users(
       id SERIAL PRIMARY KEY,
       name VARCHAR(20),
-      email VARCHAR(20) NOT NULL,
+      email VARCHAR(20) UNIQUE NOT NULL,
       password VARCHAR(20) NOT NULL,
       is_active BOOLEAN DEFAULT true,
       age INT,
@@ -45,13 +45,24 @@ app.get('/', (req: Request, res: Response) => {
 // post method
 app.post('/', async (req: Request, res: Response) => {
   // console.log(req.body)
-  const { name, email, passowrd } = req.body;
-  res.status(201).json({
-    message: "Created",
-    data: {
-      name, email
-    },
-  })
+  const { name, email, password, age } = req.body;
+
+  try {
+    const result = await pool.query(`
+    INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4)
+    RETURNING *
+    `, [name, email, password, age])
+    // console.log(result)
+    res.status(201).json({
+      message: "User Created Successfully!",
+      data: result.rows[0],
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    })
+  }
 })
 
 app.listen(port, () => {
