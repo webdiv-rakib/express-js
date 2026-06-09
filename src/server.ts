bd15c99ca0f61153cc255522512b6aed7930aace
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-09 14:26:44 +0600

    Delete User with Delete Method

--- src/server.ts
+++ src/server.ts
@@ -124,7 +124,13 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
   // console.log(name, password, age, is_active)
   try {
     const result = await pool.query(`
-    UPDATE users SET name=$1,password=$2,age=$3,is_active=$4
+    UPDATE users SET
+    name=COALESCE($1 ,name),
+    password=COALESCE($2 ,password),
+    age=COALESCE($3 ,age),
+    is_active=COALESCE($4, is_active)
+
+
     WHERE id=$5 RETURNING *
     `, [name, password, age, is_active, id])
 
@@ -139,6 +145,34 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
       success: true,
       message: 'User updated successfully',
       data: result.rows[0]
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    })
+  }
+})
+
+// delete method
+app.delete('/api/users/:id', async (req: Request, res: Response) => {
+  const { id } = req.params
+  try {
+    const result = await pool.query(`
+     DELETE FROM users WHERE id=$1 
+      `, [id])
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'User Not Found!',
+        data: {}
+      })
+    }
+    res.status(200).json({
+      success: true,
+      message: 'User Deleted Successfully',
+      data: {}
     })
   } catch (error: any) {
     res.status(500).json({
