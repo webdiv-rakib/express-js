505009f15ccd71522e094ddef729353dc075094d
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-12 18:29:23 +0600

    Understanding express middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { type Application, type Request, type Response } from "express"
 import { userRoute } from "./modules/user/user.route"
 import { profileRoute } from "./modules/profile/profile.route"
 import { authRoute } from "./modules/auth/auth.route";
+import fs from "fs"
 
 const app: Application = express();
 // export const port = config.port
@@ -9,6 +10,19 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log('Method - URL - Time:', req.method, req.url, Date.now());
+  const log = `
+  \nMethod -> ${req.method} 
+  \nTime -> ${Date.now()}
+  \nURL -> ${req.url}\n
+  `;
+  //to  write log 
+  fs.appendFile('logger.txt', log, (error) => {
+    console.log(error)
+  })
+  next();
+});
 
 app.use('/api/users', userRoute);
 app.use('/api/profile', profileRoute);
