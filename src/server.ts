20549f7c4298e79f22369623b0f2897195d04b4d
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-24 18:27:22 +0600

    some changes

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import { initDB } from "./db";
 const main = () => {
   initDB();
   app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-  })
-}
+    console.log(`Example app listening on port ${config.port}`);
+  });
+};
 
-main()
+main();
